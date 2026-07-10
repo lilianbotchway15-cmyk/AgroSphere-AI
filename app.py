090669d7229ceed670b26d18766669b1a2468c03
Lilian Botchway <lilianbotchway@gmail.com>
2026-07-10 22:12:11 +0000

    Updated frontend and fixed image paths

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="public/static",
+    static_url_path="/static"
+)
 
 @app.route("/")
 def home():
